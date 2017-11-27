e8b2177a954ce0482bd8fa3a8905b983b9215632
GLOBANT\miguel.sosa <miguel.sosa@globant.com>
2017-11-27 14:49:01 -0300

    Initial commit - Configured base of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// My modules
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
